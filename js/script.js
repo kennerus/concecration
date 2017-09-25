3a83e1fa04a977dbbd2ed16cbdb4941e495d6d0d
kennerus <kennerus@gmail.com>
2017-09-25 18:00:25 +0300

    Сделана первая страница, осталось добавить адаптивность

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+		// $('#nav-icon1').click(function() {
+		// 	$(this).toggleClass('open');
+		// 	$('.header__nav').toggleClass('expand');
+		// });
+		$('.header__profile-search').click(function() {
+			$('.search').toggleClass('srch');
+		});
+})
