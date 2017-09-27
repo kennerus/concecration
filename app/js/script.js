8b932f77e30559c26dfb8e95882f14698c27b940
kennerus <kennerus@gmail.com>
2017-09-27 14:06:52 +0300

    сделаны: главная, страница продукта, новости, контакты. решения в процессе

--- app/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-		// $('#nav-icon1').click(function() {
-		// 	$(this).toggleClass('open');
-		// 	$('.header__nav').toggleClass('expand');
-		// });
-		$('.header__profile-search').click(function() {
-			$('.search').toggleClass('srch');
-		});
-})
